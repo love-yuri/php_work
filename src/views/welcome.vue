3a5868f73fa4fa8a4b0b46e9324549ff55236ad3
love-yuri <yuri2078170658@gmail.com>
2024-06-04 23:43:26 +0800

    feat(welcome.vue): 添加News组件的@login事件处理函数

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,13 +1,13 @@
 <!--
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-04-11 23:06:51
- * @LastEditTime: 2024-05-24 21:25:41
+ * @LastEditTime: 2024-06-04 14:11:14
  * @Description: 欢迎界面
 -->
 <template>
   <div class="w-full h-full flex justify-center p-3">
     <Login v-if="!isLogin" @success="login" />
-    <News :user-info="userInfo" @log-out="isLogin = false" v-else />
+    <News :user-info="userInfo" @log-out="isLogin = false" @login="isLogin = false" v-else />
   </div>
 </template>
 <script setup lang="ts">
@@ -15,8 +15,13 @@ import { onMounted, ref } from 'vue';
 import { Login } from '@/components/login/index';
 import News from './news.vue';
 
-const userInfo = ref();
-const isLogin = ref(false);
+const userInfo = ref({
+  id: -1,
+  username: '',
+  root: 0,
+});
+
+const isLogin = ref(true);
 
 function login(params: any) {
   isLogin.value = true;
